c67aa63c73bf22e5c49050debfd4641f9b860632
Nikhil Inja <realninjadev@gmail.com>
2026-06-14 21:46:06 -0700

    Update Sentiment analysis (NLP 3/4)

--- foundations/sentiment.py
+++ foundations/sentiment.py
@@ -1,45 +1,24 @@
 import torch
 import torch.nn as nn
 from torchtyping import TensorType
-from typing import List
 
-class Solution:
-    def get_dataset(self, positive: List[str], negative: List[str]) -> TensorType[float]:
-        # 1. Build vocabulary: collect all unique words, sort them, assign integer IDs starting at 1
-        # 2. Encode each sentence by replacing words with their IDs
-        # 3. Combine positive + negative into one list of tensors
-        # 4. Pad shorter sequences with 0s using nn.utils.rnn.pad_sequence(tensors, batch_first=True)
-        words = []
-        for s in positive:
-            words += s.split(' ')
-        for n in negative:
-            words += n.split(' ')
-        words = list(sorted(set(words)))
-        vocab = {}
-        for i in range(len(words)):
-            vocab[words[i]] = torch.Tensor([float(i + 1)])
-        
-        pos = []
-        neg = []
+class Solution(nn.Module):
+    def __init__(self, vocabulary_size: int):
+        super().__init__()
+        torch.manual_seed(0)
+        # Layers: Embedding(vocabulary_size, 16) -> Linear(16, 1) -> Sigmoid
+        self.embedLayer = nn.Embedding(vocabulary_size, 16)
+        self.layer1 = nn.Linear(16 , 1)
+        self.activation = nn.Sigmoid()
 
-        for p in positive:
-            s = p.split(' ')
-            sentence = []
-            for w in s:
-                sentence.append(vocab[w])
-            pos.append(torch.Tensor(sentence))
+    def forward(self, x: TensorType[int]) -> TensorType[float]:
+        # Hint: The embedding layer outputs a B, T, embed_dim tensor
+        # but you should average it into a B, embed_dim tensor before using the Linear layer
 
-        for n in negative:
-            s = n.split(' ')
-            sentence = []
-            for w in s:
-                sentence.append(vocab[w])
-            neg.append(torch.Tensor(sentence))
-        
-        
-        res = pos + neg
-
-        res = nn.utils.rnn.pad_sequence(res, batch_first=True)
-
-        return res
+        # Return a B, 1 tensor and round to 4 decimal places
+        x = self.embedLayer(x)
+        x = x.mean(dim=1)
+        x = self.layer1(x)
+        x = self.activation(x)
+        return x
         
